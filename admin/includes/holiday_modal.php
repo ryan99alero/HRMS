dc827a4934e935fdc45f7ffd39d95c200a37f9fe
abdullahinvoisolution <abdullahinovisol@gmail.com>
2023-09-26 20:06:44 +0500

    2023-09-26

--- admin/includes/holiday_modal.php
+++ admin/includes/holiday_modal.php
@@ -110,6 +110,7 @@
         </div>
     </div>
 </div>
+
 <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function() {
