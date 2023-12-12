098c582ed7056326d37f75fa1ac9e36795336787
Ryan99alero <ryan@randgraphics.com>
2023-12-11 23:50:10 -0600

    Updated User Table's Field named RecId to be just id.

--- hr/includes/session.php
+++ hr/includes/session.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+/*session_start();
 include 'includes/conn.php';
 
 if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
@@ -9,5 +9,31 @@ if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
 $sql = "SELECT * FROM user WHERE id = '" . $_SESSION['admin'] . "'";
 $query = $conn->query($sql);
 $user = $query->fetch_assoc();
+
+*/
+session_start();
+include 'includes/conn.php';
+
+if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
+    header('location: index.php');
+    exit; // Always call exit after header redirection
+}
+
+// Use prepared statement for security
+$sql = "SELECT * FROM user WHERE id = ?";
+$stmt = $conn->prepare($sql);
+
+if($stmt){
+    // Bind the parameter and execute
+    $stmt->bind_param("s", $_SESSION['admin']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    $stmt->close();
+} else {
+    // Error handling
+    die("Database error: " . $conn->error);
+}
 
 ?>
