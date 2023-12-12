0a645370762ab0c8e45cbd1b60b9e8b3816d5c05
Ryan99alero <ryan@randgraphics.com>
2023-12-12 13:53:01 -0600

    Updated login.php page.

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 
 // Retrieve environment variables
 $dbHost = getenv('DB_HOST') ?: 'localhost'; // Default to 'localhost' if not set
-$dbName = getenv('DB_NAME') ?: 'hrms';      // Default to 'hrms' if not set
+$dbName = getenv('DB_NAME') ?: 'attendance';      // Default to 'hrms' if not set
 $dbUser = getenv('DB_USER') ?: 'phpmyadmin';      // Default to 'root' if not set
 $dbPass = getenv('DB_PASS') ?: 'KnzudGNfJoiQgKv3nUNY37';          // Default to an empty string if not set
 
