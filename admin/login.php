ddd5c9898b995413864a7a270c8015a296ce4174
Ryan99alero <ryan@randgraphics.com>
2023-12-12 09:06:27 -0600

    Updated User Table's Field named RecId to be just id.

--- admin/login.php
+++ admin/login.php
@@ -13,14 +13,13 @@ if(isset($_POST['login'])){
 
     if ($stmt) {
         // Bind parameters and execute
-        $stmt->bind_param("ss", $username);
-//            , $password);
+        $stmt->bind_param("ss", $username,$password);
         $stmt->execute();
 
         // Get the result
         $result = $stmt->get_result();
 
-        if ($result->num_rows < 1) {
+        if ($result->num_rows < 0) {
             $_SESSION['error'] = 'Username ' . htmlspecialchars($username) . ' does not exist';
         } else {
             $row = $result->fetch_assoc();
