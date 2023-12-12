f5e9eaab9fa45e094a5427fe5b4cffcbaeba6fc8
Ryan99alero <ryan@randgraphics.com>
2023-12-12 13:51:45 -0600

    Updated login.php page.

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -5,8 +5,8 @@ error_reporting(E_ALL);
 // Retrieve environment variables
 $dbHost = getenv('DB_HOST') ?: 'localhost'; // Default to 'localhost' if not set
 $dbName = getenv('DB_NAME') ?: 'hrms';      // Default to 'hrms' if not set
-$dbUser = getenv('DB_USER') ?: 'root';      // Default to 'root' if not set
-$dbPass = getenv('DB_PASS') ?: '';          // Default to an empty string if not set
+$dbUser = getenv('DB_USER') ?: 'phpmyadmin';      // Default to 'root' if not set
+$dbPass = getenv('DB_PASS') ?: 'KnzudGNfJoiQgKv3nUNY37';          // Default to an empty string if not set
 
 // Create a new connection
 $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
