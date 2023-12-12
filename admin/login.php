5a9c6be935b9a219bd56671d6a85955a3d5e633b
Ryan99alero <ryan@randgraphics.com>
2023-12-12 13:44:21 -0600

    Updated login.php page.

--- admin/login.php
+++ admin/login.php
@@ -1,9 +1,3 @@
-//https://www.sourcecodester.com/php-project
-//https://www.codecademy.com/learn
-//https://www.geeksforgeeks.org/web-development-projects/
-//https://stackoverflow.com/
-//https://bootstrap.com/docs/5.3/getting-started/introduction/
-
 <?php
 session_start();
 include 'includes/conn.php';
