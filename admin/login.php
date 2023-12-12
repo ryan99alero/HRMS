720b0d6996b8635f8f04889ff6bca802b63a3596
Ryan99alero <ryan@randgraphics.com>
2023-12-12 02:32:57 -0600

    Updated User Table's Field named RecId to be just id.

--- admin/login.php
+++ admin/login.php
@@ -27,6 +27,8 @@ if (isset($_POST['login'])) {
 }
 
 header('location: index.php');*/
+
+
 session_start();
 include 'includes/conn.php';
 
@@ -34,25 +36,39 @@ if(isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    //$sql = "SELECT * FROM user WHERE username = '$username'";
-    $sql = "call 'StrProc_getUserLoginInfo'('".$_POST["username"]."','".$_POST["password"]."')";
-    $query = $conn->query($sql);
+    // Using prepared statements to call stored procedure
+    $sql = "CALL StrProc_getUserLoginInfo(?, ?)";
+    $stmt = $conn->prepare($sql);
 
-    if($query->num_rows < 1){
-        $_SESSION['error'] = 'Cannot find account with the username';
-    }
-    else{
-        $row = $query->fetch_assoc();
-        if(password_verify($password, $row['password'])){
-            $_SESSION['admin'] = $row['id'];
+    if ($stmt) {
+        // Bind parameters and execute
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+
+        // Get the result
+        $result = $stmt->get_result();
+
+        if ($result->num_rows < 1) {
+            $_SESSION['error'] = 'Cannot find account with the username';
+        } else {
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row['password'])) {
+                $_SESSION['admin'] = $row['id'];
+            } else {
+                $_SESSION['error'] = 'Incorrect password';
+            }
         }
-        else{
-            $_SESSION['error'] = 'Incorrect password';
-        }
+
+        // Close the statement
+        $stmt->close();
+    } else {
+        // Handle error
+        echo "Error in preparing statement: " . $conn->error;
     }
 
-}
-else{
+    // Close the database connection
+    $conn->close();
+} else {
     $_SESSION['error'] = 'Input admin credentials first';
 }
 
