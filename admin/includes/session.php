fa4fbbd40284689f6280debc303a8cbc59eceaea
Ryan99alero <ryan@randgraphics.com>
2023-12-12 01:08:55 -0600

    Updated User Table's Field named RecId to be just id.

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -1,43 +1,36 @@
 <?php
-/*session_start();
-include 'includes/conn.php';
-
-// Check if the 'admin' session variable is set and not empty
-if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
-    header('Location: index.php'); // Replace 'index.php' with your login page
-    exit();
-}
-
-$sql = "SELECT * FROM user WHERE id = '" . $_SESSION['admin'] . "'";
-$query = $conn->query($sql);
-$user = $query->fetch_assoc();
-
-// Rest of your code...
-*/
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', '/path/to/error_log.txt'); // Update with your actual server path
 
 session_start();
 include 'includes/conn.php';
 
-if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
+if (!isset($_SESSION['admin']) || trim($_SESSION['admin']) == '') {
     header('location: index.php');
-    exit; // Always call exit after header redirection
+    exit;
 }
 
-// Use prepared statement for security
+// Debugging statement
+echo 'Admin session value: ' . $_SESSION['admin'] . '<br>';
+
 $sql = "SELECT * FROM user WHERE id = ?";
 $stmt = $conn->prepare($sql);
 
-if($stmt){
-    // Bind the parameter and execute
+if ($stmt) {
     $stmt->bind_param("s", $_SESSION['admin']);
     $stmt->execute();
     $result = $stmt->get_result();
-    $user = $result->fetch_assoc();
-
+    if ($result) {
+        $user = $result->fetch_assoc();
+        // Debugging statement
+        echo '<pre>'; var_dump($user); echo '</pre>';
+    } else {
+        echo "Error in result fetching: " . $conn->error;
+    }
     $stmt->close();
 } else {
-    // Error handling
-    die("Database error: " . $conn->error);
+    die("Error in query preparation: " . $conn->error);
 }
-
 ?>
