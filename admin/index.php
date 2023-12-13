93e8f3db7078f847eded5666db93100b0337e63b
Ryan99alero <ryan@randgraphics.com>
2023-12-12 19:17:07 -0600

    Updated login.php page.

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-if (isset($_SESSION['admin'])) {
+if(isset($_SESSION['admin'])){
     header('location:home.php');
 }
 ?>
@@ -10,47 +8,41 @@ if (isset($_SESSION['admin'])) {
 <body class="hold-transition login-page">
 <div class="login-box">
     <div class="login-logo">
-        <p>Admin</p>
+        <b>Admin Login</b>
     </div>
+
     <div class="login-box-body">
         <p class="login-box-msg">Sign in to start your session</p>
 
         <form action="login.php" method="POST">
             <div class="form-group has-feedback">
-                <label>
-                    <input type="text" class="form-control" name="username" placeholder="input Username" required autofocus>
-                </label>
+                <input type="text" class="form-control" name="username" placeholder="input Username" required autofocus>
                 <span class="glyphicon glyphicon-user form-control-feedback"></span>
             </div>
             <div class="form-group has-feedback">
-                <label>
-                    <input type="password" class="form-control" name="password" placeholder="input Password" required>
-                </label>
+                <input type="password" class="form-control" name="password" placeholder="input Password" required>
                 <span class="glyphicon glyphicon-lock form-control-feedback"></span>
             </div>
             <div class="row">
                 <div class="col-xs-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i
-                                class="fa fa-sign-in"></i> Sign In
-                    </button>
+                    <button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
                 </div>
                 <div class="col-xs-2">
                     <!-- <button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button> -->
                 </div>
                 <div class="col-xs-6">
                     <a href="../hr/index.php">
-                        <btn class="btn btn-success btn-block btn-flat"><i class="fa fa-sign-in"></i> Sign In to HR
-                        </btn>
+                        <btn  class="btn btn-success btn-block btn-flat"><i class="fa fa-sign-in"></i> Sign In to HR</btn>
                     </a>
                 </div>
             </div>
         </form>
     </div>
     <?php
-    if (isset($_SESSION['error'])) {
+    if(isset($_SESSION['error'])){
         echo "
   				<div class='callout callout-danger text-center mt20'>
-			  		<p>" . $_SESSION['error'] . "</p> 
+			  		<p>".$_SESSION['error']."</p> 
 			  	</div>
   			";
         unset($_SESSION['error']);
