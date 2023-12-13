a863a1c95955896c20c214dc0505de30b7a35d70
Ryan99alero <ryan@randgraphics.com>
2023-12-12 19:13:59 -0600

    Updated login.php page.

--- admin/login.php
+++ admin/login.php
@@ -16,24 +16,28 @@ if(isset($_POST['login'])){
 
     if($result->num_rows < 1){
         $_SESSION['error'] = 'Cannot find account with the username';
+        header('location: index.php'); // Redirect to login page
+        exit();
     }
     else{
         $row = $result->fetch_assoc();
         if(password_verify($password, $row["password"])){
             $_SESSION['admin'] = $row['id'];
+            $_SESSION['success'] = 'Login successful'; // Set a success message
+            header('location: home.php'); // Redirect to home page
+            exit();
         }
         else{
             $_SESSION['error'] = 'Incorrect password';
-            header('location: home.php'); // Redirect should be done after setting the session variable
-            exit(); // Always call exit after header redirection
+            header('location: index.php'); // Redirect to login page
+            exit();
         }
     }
     $stmt->close();
 }
 else{
     $_SESSION['error'] = 'Input user credentials first';
+    header('location: index.php');
+    exit();
 }
-
-header('location: index.php');
-exit(); // Always call exit after header redirection
 ?>
